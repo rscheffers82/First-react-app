2e74cf725bf883067a2fc9701b6b37b94cae1a87
Roy Scheffers <r.scheffers@gmail.com>
2016-11-18 16:02:47 -0500

    Add message update + checks

--- app.jsx
+++ app.jsx
@@ -1,22 +1,46 @@
 var Greeter = React.createClass({
   getDefaultProps: function(){
     return {
-      name: 'React'
+      name: 'React',
+      message: 'No message provided so display the default one'
     };
   },
+  getInitialState: function(){
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+    if (typeof name === 'string' && name.length > 0){
+      this.setState({
+        name: name
+      });
+    }
+  },
   render: function(){
-    var name = this.props.name;
+    var name = this.state.name;
+    var message = this.props.message;
     return(
       <div>
         <h1>Hello {name}!</h1>
-        <p>Text from a paragraph within an component</p>
+        <p>{message}</p>
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
-})
+});
 
+var fullName = 'Roy Scheffers';
+var shortMessage = 'Mastering React already, look at the Ninja :)'
 
 ReactDOM.render(
-  <Greeter name='Roy Scheffers'/>,
+  <Greeter name={fullName} message={shortMessage}/>,
   document.getElementById('app')
 );
