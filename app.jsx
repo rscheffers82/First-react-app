0bba16636d9de6cc1d1e149f22d2b10b31f8e952
Roy Scheffers <r.scheffers@gmail.com>
2016-11-18 15:03:43 -0500

    First component + props added :)

--- app.jsx
+++ app.jsx
@@ -1,5 +1,22 @@
+var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return {
+      name: 'React'
+    };
+  },
+  render: function(){
+    var name = this.props.name;
+    return(
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>Text from a paragraph within an component</p>
+      </div>
+    );
+  }
+})
+
 
 ReactDOM.render(
-  <h1>Hello React2!</h1>,
+  <Greeter name='Roy Scheffers'/>,
   document.getElementById('app')
 );
